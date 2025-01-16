ecf558c7f758e402c8dc8e9cd633a7c30ab42485
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-01-16 16:19:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+// cosnt table1 = document.querySelector('.tad');
+const tdItem = document.querySelectorAll('.tad td');
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  let sum = 0;
+	tdItem.forEach(td =>{
+		const value = parseFloat(td.textContent);
+		if(!isNaN(value)){
+			sum += value;
+		}
+	})
+
+
+const result = document.createElement('tr')
+result.innerHTML =`<td colspan= '100%'>Total: ${sum}</td>`
+
+	const table = document.querySelector('.tad');
+	table.appendChild(result);
+
 };
 
 getSumBtn.addEventListener("click", getSum);
